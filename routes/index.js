2f097b2fb451c758dae8743c579d86119816b36c
kryptoxdev <kryptoxmail@proton.me>
2024-09-04 21:18:10 +0100

    Cleaned up, started adding condition to resemble Titan

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../public/javascripts/iphoneModels.json')
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
 	res.render('index', {
-		title: 'Website Playground'
+		title: 'Website Playground',
+		modelArray: models
 	});
 });
 
 router.post("/", (req, res) => {
 	const colour = req.body.colour;
 	const deviceIMEI = req.body.deviceIMEI;
+	const model = req.body.model;
+	const condition = req.body.condition;
 	
 	console.log(req.body);
 	
-	if (!colour || !deviceIMEI) {
+	if (!colour || !deviceIMEI || !model || !condition) {
 		res.status(400).send('Invalid form data');
 	} else {
 		res.send("Form sent");
