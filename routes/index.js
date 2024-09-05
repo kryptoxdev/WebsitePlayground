8f4b43498051b63ca0ecc0392f75dfe330b10b30
kryptoxfps <kryptoxmail@proton.me>
2024-09-05 12:34:02 +0100

    Finished condition inputs, more tidying

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var router = express.Router();
-var models = require('../public/javascripts/iphoneModels.json')
+var models = require('../public/javascripts/iphoneModels.json');
+var carriers = require("../public/javascripts/carriers.json");
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
 	res.render('index', {
 		title: 'Website Playground',
-		modelArray: models
+		modelArray: models,
+		carriers: carriers
 	});
 });
 
