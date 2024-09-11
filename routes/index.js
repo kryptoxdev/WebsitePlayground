99081c97d43244194cc73b6406233ea873e7a0d7
kryptoxdev <kryptoxmail@proton.me>
2024-09-11 21:13:09 +0100

    Made homepage 'phonecheck', implemented actual form redirect

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var models = require('../public/javascripts/iphoneModels.json');
 var carriers = require("../public/javascripts/carriers.json");
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/phonecheck', function (req, res, next) {
 	res.render('index', {
 		title: 'Website Playground',
 		modelArray: models,
@@ -12,7 +12,13 @@ router.get('/', function (req, res, next) {
 	});
 });
 
-router.post("/", (req, res) => {
+router.get("/formReturn", function (req, res, next) {
+	res.render('formReturn', {
+		title: 'Form Return'
+	});
+});
+
+router.post("/formReturn", (req, res) => {
 	const colour = req.body.colour;
 	const deviceIMEI = req.body.deviceIMEI;
 	const model = req.body.model;
